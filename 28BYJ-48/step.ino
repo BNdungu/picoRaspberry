f797a90a777f0184f4751a5b567aa175d4a5379f
BNdungu <bryannganga909@gmail.com>
2023-11-28 21:40:15 +0300

    _

--- 28BYJ-48/step.ino
+++ 28BYJ-48/step.ino
@@ -1,25 +1,16 @@
-//Includes the Arduino Stepper Library
 #include <Stepper.h>
 
-// Defines the number of steps per rotation
 const int stepsPerRevolution = 2038;
 
-// Creates an instance of stepper class
-// Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
 Stepper myStepper = Stepper(stepsPerRevolution, 0, 2, 1, 3);
 
 void setup() {
-    // Nothing to do (Stepper Library sets pins as outputs)
 }
 
 void loop() {
-	// Rotate CW slowly at 5 RPM
 	myStepper.setSpeed(5);
 	myStepper.step(238);
-	//delay(100);
 	
-	// Rotate CCW quickly at 10 RPM
 	myStepper.setSpeed(15);
 	myStepper.step(-538);
-	//delay(1000);
 }
